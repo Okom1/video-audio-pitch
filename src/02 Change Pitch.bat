f8bb0fa55d3db876d74929b9911243ac5f009afd
Okom1 <okomokomok@gmail.com>
2025-11-18 19:17:16 +0200

    feat: Inherit input file extension Changed output file extension to not always be .mp4, and instead use the input file extension.

--- src/02 Change Pitch.bat
+++ src/02 Change Pitch.bat
@@ -2,5 +2,5 @@
 
 set /p multiplier="Enter semitone change (e.g. -2): "
 
-ffmpeg -i "%~1" -af rubberband=pitch=1.059463094359295^^%multiplier% -c:v copy "%~n1 (%multiplier%).mp4"
+ffmpeg -i "%~1" -af rubberband=pitch=1.059463094359295^^%multiplier% -c:v copy "%~n1 (%multiplier%)%~x1"
 exit
